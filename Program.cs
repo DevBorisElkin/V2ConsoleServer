317ea452a02bbe369eaa80766a2f3667846311cb
DevBorisElkin <ea.foreverrr@gmail.com>
2021-07-23 12:06:35 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace V2ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
